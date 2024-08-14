d1778f153aa175e00e66db95afcf71981268317b
ahsanreaz120363 <ahsanreaz120363@gmail.com>
2024-08-14 12:14:36 +0600

    khudalagce server setup and running

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const port = process.env.PORT || 5000;
+
+// middleware 
+app.use(cors());
+app.use(express.json())
+
+
+app.get('/',(req, res)=>{
+    res.send("khudalagce is set up");
+})
+
+app.listen(port, ()=>{
+    console.log(`khudalagce is running on port ${port}`);
+})
